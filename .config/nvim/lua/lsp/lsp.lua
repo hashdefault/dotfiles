d4e59a14c09db795f6f5df147252815d5e4574f8
Lucas Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-06-07 15:37:34 -0300

    icons in lsp diagnostic
    Signed-off-by: Lucas Gabriel Bueno <lbueno.gabriel@gmail.com>

--- .config/nvim/lua/lsp/lsp.lua
+++ .config/nvim/lua/lsp/lsp.lua
@@ -23,3 +23,11 @@ lspconfig.stylelint_lsp.setup {}
 lspconfig.html.setup {}
 lspconfig.cmake.setup {}
 
+
+-- symbols to lsp dignostic
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
