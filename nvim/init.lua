ecff8167c3d335b49b7bf6161f2c661dcbae57f2
Lucas Gabriel Bueno <lbueno.gabriel@gmail.com>
2022-06-22 17:57:40 -0300

    nvim tree and telescope configs

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+
+require 'plugins'
+require 'settings'
+require 'lsp.lsp'
+require 'plugin.treesitter-config'
+require 'plugin.compe-config'
+require 'plugin.airline-config'
+require 'plugin.telescope-config'
+require 'plugin.nvimtree-config'
