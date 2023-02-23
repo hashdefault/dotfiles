a591c9e41927f80950da1f2af6754810e8b11177
Lucas G. Bueno <lbueno.gabriel@gmail.com>
2023-02-23 08:15:37 -0300

    updt

---
+++ xmonad/scripts/autostart.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Set your native resolution IF it does not exist in xrandr
+#More info in the script
+#run $HOME/.xmonad/scripts/set-screen-resolution-in-virtualbox.sh
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+
+#home
+xrandr --no-startup-id  xrandr --output HDMI-1 --primary
+xrandr --output eDP1  --mode 1366x768 --pos 1920x0  --rotate normal --output HDMI1  --mode  1920x1080  --pos 0x0 --rotate normal
+
+#agiliza
+#xrandr --output HDMI-1 --mode 1360x768 --pos 0x0 --rotate normal --output DP-1  --mode  1440x900 --pos 1360x0 --rotate normal
+
+#(sleep 2; run $HOME/.config/polybar/launch.sh) &
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+#cursor active at boot
+xsetroot -cursor_name left_ptr &
+
+#start ArcoLinux Welcome App
+#run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+
+#Some ways to set your wallpaper besides variety or nitrogen
+run nitrogen --restore
+#start the conky to learn the shortcuts
+killall conky; (conky -c $HOME/.config/xmonad/scripts/system-overview) &
+
+#starting utility applications at boot time
+run nm-applet &
+run pamac-tray &
+run volumeicon &
+numlockx on &
+blueberry-tray &
+run picom &
+
+#starting user applications at boot time
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run spotify &
+#run atom &
+
+#run telegram-desktop &
+#run discord &
+#run dropbox &
+#run insync start &
+#run ckb-next -b &
