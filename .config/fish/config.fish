2f4219a0ba592be3dc82dd99ea95b513c837d071
Lucas G. Bueno <lbueno.gabriel@gmail.com>
2023-12-14 17:21:44 -0300

    add commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+function fish_greeting
+end
 alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
 alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
 alias parsua='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
@@ -21,3 +23,5 @@ alias dockerphp8='docker container stop php7_mariadb php7_phpmyadmin php7_apache
 alias dockerphp7='docker container stop php8_mariadb php8_phpmyadmin php8_apache && docker container start php7_mariadb php7_phpmyadmin php7_apache'
 
 starship init fish | source
+
+neofetch
