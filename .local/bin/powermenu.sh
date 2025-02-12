3459c99fae82355c2f736f4511db6b6c19b6fd7d
Lucas G. Bueno <lucasgbueno@proton.me>
2025-02-12 20:46:41 -0300

    add rofi colors and powermenu customization

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -34,24 +34,20 @@ uptime=$(uptime -p | sed -e 's/up //g')
 rofi_command="rofi -theme $dir/$theme"
 
 shutdown="⏻"
-reboot="↺"
-lock="↵"
-suspend="⏾"
-logout="↵" 
-
+reboot=""
+lock=""
+suspend="󰤄"
+logout="󰗽"
 
 # Confirmation
 confirm_exit() {
-	rofi -dmenu\
-		-i\
-		-no-fixed-num-lines\
-		-p "Are You Sure? : "\
-		-theme $dir/confirm.rasi
+  rofi -dmenu -i -no-fixed-num-lines -p "Are You Sure? : " \
+    -theme $dir/confirm.rasi
 }
 
 # Message
 msg() {
-	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+  rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
 }
 
 # Variable passed to rofi
@@ -59,50 +55,50 @@ options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
 case $chosen in
-    $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $lock)
-		betterlockscreen -l
-        ;;
-    $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			playerctl pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			session=`loginctl session-status | head -n 1 | awk '{print $1}'`
-			loginctl terminate-session $session
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
+$shutdown)
+  ans=$(confirm_exit &)
+  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+    systemctl poweroff
+  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+    exit 0
+  else
+    msg
+  fi
+  ;;
+$reboot)
+  ans=$(confirm_exit &)
+  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+    systemctl reboot
+  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+    exit 0
+  else
+    msg
+  fi
+  ;;
+$lock)
+  betterlockscreen -l
+  ;;
+$suspend)
+  ans=$(confirm_exit &)
+  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+    playerctl pause
+    amixer set Master mute
+    systemctl suspend
+  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+    exit 0
+  else
+    msg
+  fi
+  ;;
+$logout)
+  ans=$(confirm_exit &)
+  if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+    session=$(loginctl session-status | head -n 1 | awk '{print $1}')
+    loginctl terminate-session $session
+  elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+    exit 0
+  else
+    msg
+  fi
+  ;;
 esac
