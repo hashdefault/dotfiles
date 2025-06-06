79ad1077ab2819daea78d8f64ee6102decd0f390
Lucas G. Bueno <lucasgbueno@proton.me>
2025-06-06 15:14:28 -0300

    add pywal to generate colorschemes based on wallpapers

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -5,10 +5,12 @@ INTERVAL=1800 # time in seconds to change
 WALLPAPERS_DIR="$HOME/Pictures/wallpapers"
 
 while true; do
-  RESPONSE=$(swww img "$(find "$WALLPAPERS_DIR" -type f | shuf -n1)" --transition-fps 60 --transition-type wipe 2>&1)
+
+  wallpaper_path=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" -o -iname "*.svg" \) | shuf -n1)
+  RESPONSE=$(swww img $wallpaper_path --transition-fps 60 --transition-type wipe 2>&1)
   if echo $RESPONSE | grep -iq "Error"; then
     continue
   fi
-
+  wal -i $wallpaper_path
   sleep "$INTERVAL"
 done
