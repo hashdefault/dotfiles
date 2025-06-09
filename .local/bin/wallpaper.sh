ff390a108fdcdba1fe1985388edf2d292c29f307
Lucas G. Bueno <lucasgbueno@proton.me>
2025-06-08 21:09:47 -0300

    updt script to sync pywal with ghostty terminal

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
 INTERVAL=1800 # time in seconds to change
-
 WALLPAPERS_DIR="$HOME/Pictures/wallpapers"
+LAST_WALLPAPER=""
 
 while true; do
-
   wallpaper_path=$(find "$WALLPAPERS_DIR" -type f \( -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" -o -iname "*.svg" \) | shuf -n1)
-  swww img $wallpaper_path --transition-fps 60 --transition-type wipe 2>&1
 
-  sleep 2
-  wal -qi $wallpaper_path
+  # Skip if same as last wallpaper
+  if [ "$wallpaper_path" = "$LAST_WALLPAPER" ]; then
+    sleep 1
+    continue
+  fi
+
+  swww img "$wallpaper_path" --transition-fps 60 --transition-type wipe
+
+  # Only run wal if wallpaper actually changed
+  wal -qi "$wallpaper_path"
+  LAST_WALLPAPER="$wallpaper_path"
+
+  ~/.local/bin/update-ghostty-wal-colors.sh
 
   sleep "$INTERVAL"
 done
