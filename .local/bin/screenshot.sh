b0bd50f2b0fb6ab91c1fa157b123e52cfef6a216
Lucas G. Bueno <lucasgbueno@proton.me>
2025-06-15 22:01:16 -0300

    updt scripts eww scss

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -3,12 +3,11 @@
 # Create a temporary file for the screenshot
 tmpfile=$(mktemp --suffix=.png)
 
-
 # Take a snapshot of the clipboard before taking the screenshot
 initial_clipboard=$(wl-paste --list-types)
 
 # Take the screenshot
-grim -g "$(slurp)" "$tmpfile" && swappy -f "$tmpfile" 
+grim -g "$(slurp)" "$tmpfile" && swappy -f "$tmpfile"
 
 sleep 1
 
@@ -17,5 +16,5 @@ current_clipboard=$(wl-paste --list-types)
 
 # If the clipboard now contains an image and it's different from the initial state
 if echo "$current_clipboard" | grep -q "image/" && [ "$initial_clipboard" != "$current_clipboard" ]; then
-    notify-send "Screenshot Copied to the clipboard "
+  notify-send -i shoot "Screenshot" "Copied to the clipboard"
 fi
