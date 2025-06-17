bf06fbe6e63b38a4fcf747768634adde18bb4b9e
Lucas G. Bueno <lucasgbueno@proton.me>
2025-06-17 01:01:44 -0300

    updt

--- .config/eww/scripts/do_not_disturb.sh
+++ .config/eww/scripts/do_not_disturb.sh
@@ -3,10 +3,10 @@
 case $(dunstctl is-paused) in
 true)
   dunstctl set-paused false &
-  dunstify -i notification-active -a "System" "Do Not Disturb" "Turned Off" &
+  dunstify -i ~/.config/eww/images/bell.svg -a "System" "Do Not Disturb" "Turned Off" &
   ;;
 false)
-  dunstify -i notification-inactive -a "System" "Do Not Disturb" "Active" &
+  dunstify -i ~/.config/eww/images/bell-off.svg -a "System" "Do Not Disturb" "Active" &
   (sleep 3 && dunstctl close && dunstctl set-paused true)
   ;;
 esac
