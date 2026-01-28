a9336be8dd1550bb9b61afb15d7216721afb7d85
Lucas G. Bueno <lucasgbueno@proton.me>
2026-01-28 17:50:14 -0300

    updt

--- .local/bin/clipboard-dmenu.sh
+++ .local/bin/clipboard-dmenu.sh
@@ -3,6 +3,10 @@
 # 2. Pipe into dmenu (using your colors)
 # 3. Decode the selection and copy it back to clipboard
 
-cliphist list | dmenu -fn "JetBrainsMono Nerd Font:size=10" -l 10 -p "Clipboard:" \
-  -nb '#121222' -nf '#dcdcdc' -sb '#00ff99' -sf '#121222' | \
-  cliphist decode | wl-copy
+
+selected=$(cliphist list | sed 's/^[^\t]*\t//' | dmenu -fn "JetBrainsMono Nerd Font:size=10" -l 10 -p "Clipboard:" \
+  -nb '#121222' -nf '#dcdcdc' -sb '#00ff99' -sf '#121222')
+
+if [ -n "$selected" ]; then
+    cliphist list | grep -F "$selected" | head -n1 | cliphist decode | wl-copy
+fi
