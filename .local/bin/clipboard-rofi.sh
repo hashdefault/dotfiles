883bf8364276381de2e28fefdd4bab92fb0ea783
Lucas G. Bueno <lucasgbueno@proton.me>
2026-02-19 10:39:29 -0300

    updt

--- .local/bin/clipboard-rofi.sh
+++ .local/bin/clipboard-rofi.sh
@@ -11,6 +11,5 @@ fi
 # Print the selected line so it can be captured by callers if needed.
 printf '%s\n' "${selection}"
 
-# Copy selection to both clipboards.
-printf '%s' "${selection}" | wl-copy
-printf '%s' "${selection}" | wl-copy --primary
+# Copy selection to clipboard.
+printf '%s' "${selection}" | xclip -selection clipboard
