2ee412cae042b86df77735e52bffbf5c66098a96
Lucas G. Bueno <lucasgbueno@proton.me>
2026-03-26 10:49:12 -0300

    updt

--- .config/dunst/scripts/dunst_logger.sh
+++ .config/dunst/scripts/dunst_logger.sh
@@ -18,7 +18,7 @@ elif [[ "$appname" == "Calendar" ]] || [[ "$appname" == "Volume" ]] || [[ "$appn
   exit 0
 fi
 
-notification_item="$1;$2;$3;$4;$5;$6;$7;$8;$9;${10}"
+notification_item="$timestamp;$urgency;$icon;$body;$summary;$appname"
 
 tmp=$(mktemp)
 echo "$notification_item" >"$tmp"
